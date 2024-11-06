39871ffdb626ff449d48a3b1bf619ff8929f5e7b
IDG <i.zamaninasab@gmail.com>
2024-11-06 22:06:07 +0330

    add_from_uartTest

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,23 @@
 #define I2C_ADDRESS    0x19
   DFRobot_AirQualitySensor particle(&Wire ,I2C_ADDRESS);
 
-uint8_t cmdID[2];
-uint8_t length[2];
-uint8_t reserved[2] = {0x00 , 0x00};
-uint8_t checksum[2];
-uint8_t payload[10];
+#define RQST_HDR_LENGTH     sizeof(uartRqstHeader_t)
 uint8_t buffHeader[18];
 union a16to8 {
 	 uint16_t a16;
 	 uint8_t a8[2];
 };
-uint16_t crc_generate(uint8_t *buffer, size_t length, uint16_t startValue );
-uint8_t MPS_status();
-uint8_t MPS_version();
-uint8_t MPS_start_meas();
-uint8_t MPS_ans();
+typedef struct {
+  uint16_t cmdID;
+  uint16_t length;
+  uint16_t reserved;
+  uint16_t cksum;
+} uartRqstHeader_t;
+uint16_t crc_generate(uint8_t *buffer, size_t length, uint16_t startValue);
+uint8_t MPS_status(uint8_t cmdID, uint8_t *payload, uint16_t payloadLen);
+uint8_t MPS_version(uint8_t cmdID, uint8_t *payload, uint16_t payloadLen);
+//uint8_t MPS_start_meas();
+//uint8_t MPS_ans();
 void setup() {
   Serial.begin(9600);
   Serial2.begin(38400);
@@ -49,14 +51,16 @@ void setup() {
   delay(3000);
   uint8_t error;
   do {
-    error = MPS_status();
+    error = MPS_status(0x41, NULL, 0);
   }while (error != 0);
   do {
-    error = MPS_version();
+    error = MPS_version(0x42, NULL, 0);
   }while (error != 0);
+  /*
   do {
     error = MPS_start_meas();
   }while (error != 0);
+  */
   delay(2000);
 }
 
@@ -74,7 +78,7 @@ void loop() {
   Serial.print("PM1.0 concentration:");
   Serial.print(concentration);
   Serial.println(" mg/m3");
-  MPS_ans();
+  //MPS_ans();
   delay(1000);
 }
 
@@ -94,23 +98,22 @@ uint16_t crc_generate(uint8_t *buffer, size_t length, uint16_t startValue ) {
 
 }
 
-uint8_t MPS_status() {
-  union a16to8 checksumBytes;
-  cmdID[1] = 0x41;
-  cmdID[0] = 0x00;
-  length[1] = 0x00;
-  length[0] = 0x00;
-  memcpy(buffHeader, cmdID, 2);
-  memcpy(buffHeader + 2, length, 2);
-  memcpy(buffHeader + 4, reserved, 2);
-  checksumBytes.a16 = crc_generate(buffHeader, 6, 0xFFFF);
-  memcpy(buffHeader + 6, checksumBytes.a8, 2);
+uint8_t MPS_status(uint8_t cmdID, uint8_t *payload, uint16_t payloadLen) {
+  uartRqstHeader_t header;
+  uint16_t cksum, rxCksum, length;
+
+  memset(&header, 0, RQST_HDR_LENGTH);
+  header.cmdID = cmdID;
+  header.length = payloadLen;
+
+  cksum = crc_generate((uint8_t *) &header, RQST_HDR_LENGTH, 0xFFFF);
+  header.cksum = cksum;
   Serial.println("MPS status: ");
-  for (int i = 0; i < 8; i++) {
-    Serial.printf("0x%02X ", buffHeader[i]);
+  for (int i = 0; i < RQST_HDR_LENGTH; i++) {
+    Serial.printf("0x%02X ", (uint8_t *) &header);
   }
   Serial.println();
-  Serial2.write(buffHeader, 8);
+  Serial2.write((uint8_t *) &header, RQST_HDR_LENGTH);
   delay(100);
   Serial.println("MPS status reply: ");
   uint8_t ret;
@@ -122,35 +125,33 @@ uint8_t MPS_status() {
   return ret;
 }
 
-uint8_t MPS_version() {
-  union a16to8 checksumBytes;
-  cmdID[1] = 0x42;
-  cmdID[0] = 0x00;
-  length[1] = 0x00;
-  length[0] = 0x00;
-  memcpy(buffHeader, cmdID, 2);
-  memcpy(buffHeader + 2, length, 2);
-  memcpy(buffHeader + 4, reserved, 2);
-  checksumBytes.a16 = crc_generate(buffHeader, 6, 0xFFFF);
-  memcpy(buffHeader + 6, checksumBytes.a8, 2);
-  Serial.println("MPS version: ");
-  for (int i = 0; i < 8; i++) {
-    Serial.printf("0x%02X ", buffHeader[i]);
+uint8_t MPS_version(uint8_t cmdID, uint8_t *payload, uint16_t payloadLen) {
+  uartRqstHeader_t header;
+  uint16_t cksum, rxCksum, length;
+
+  memset(&header, 0, RQST_HDR_LENGTH);
+  header.cmdID = cmdID;
+  header.length = payloadLen;
+
+  cksum = crc_generate((uint8_t *) &header, RQST_HDR_LENGTH, 0xFFFF);
+  header.cksum = cksum;
+  Serial.println("MPS status: ");
+  for (int i = 0; i < RQST_HDR_LENGTH; i++) {
+    Serial.printf("0x%02X ", (uint8_t *) &header);
   }
   Serial.println();
-  Serial2.write(buffHeader, 8);
+  Serial2.write((uint8_t *) &header, RQST_HDR_LENGTH);
   delay(100);
-  Serial.println("MPS version reply: ");
+  Serial.println("MPS status reply: ");
   uint8_t ret;
   while (Serial2.available()) {
     ret = Serial2.read();
     Serial.printf("0x%02X ", ret);
   }
   Serial.println();
-  ret = 0;
   return ret;
 }
-
+/*
 uint8_t MPS_start_meas() {
   union a16to8 checksumBytes;
   cmdID[1] = 0x61;
@@ -165,11 +166,11 @@ uint8_t MPS_start_meas() {
   uint8_t command = 0x12;
   memcpy(buffHeader + 8, &command, 1);
   Serial.println("MPS start meas: ");
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 9; i++) {
     Serial.printf("0x%02X ", buffHeader[i]);
   }
   Serial.println();
-  Serial2.write(buffHeader, 8);
+  Serial2.write(buffHeader, 9);
   delay(100);
   Serial.println("MPS start meas reply: ");
   uint8_t ret;
@@ -210,3 +211,4 @@ uint8_t MPS_ans() {
   ret = 0;
   return ret;
 }
+*/
